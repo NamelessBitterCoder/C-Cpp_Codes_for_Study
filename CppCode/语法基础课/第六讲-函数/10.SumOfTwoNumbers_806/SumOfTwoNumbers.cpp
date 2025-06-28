e7fe5ec9bdf99e04ac311151fd24d6bc7da1c47f
NamelessBitterCoder <liangyiyingchen@outlook.com>
2025-06-28 22:54:49 +0800

    两个数的和-806

---
+++ CppCode/语法基础课/第六讲-函数/10.SumOfTwoNumbers_806/SumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+double add(double x, double y)
+{
+    return x + y;
+}
+int main()
+{
+    double x, y;
+    cin >> x >> y;
+    printf("%.2lf\n", add(x, y));
+    return 0;
+}
