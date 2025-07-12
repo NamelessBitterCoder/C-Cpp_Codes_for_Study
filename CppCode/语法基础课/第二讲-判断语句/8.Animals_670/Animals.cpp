f4c46bb66941e8974cb9afc0ba933c2023a72dab
NamelessBitterCoder <liangyiyingchen@outlook.com>
2025-07-12 17:01:46 +0800

    动物-670

---
+++ CppCode/语法基础课/第二讲-判断语句/8.Animals_670/Animals.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    string a, b ,c;
+    cin >> a >> b >> c;
+    if(a == "vertebrado")
+    {
+        if(b == "ave")
+        {
+            if(c == "carnivoro")
+            {
+                printf("aguia\n");
+            }
+            else
+            {
+                printf("pomba\n");
+            }
+        }
+        else
+        {
+            if(c == "onivoro")
+            {
+                printf("homem\n");
+            }
+            else
+            {
+                printf("vaca\n");
+            }
+        }
+    }
+    else
+    {
+         if(b == "inseto")
+        {
+            if(c == "hematofago")
+            {
+                printf("pulga\n");
+            }
+            else
+            {
+                printf("lagarta\n");
+            }
+        }
+        else
+        {
+            if(c == "hematofago")
+            {
+                printf("sanguessuga\n");
+            }
+            else
+            {
+                printf("minhoca\n");
+            }
+        }
+    }
+    return 0;
+}
