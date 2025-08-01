530285eb111305ea7ff074957ce241118e1ca186
NamelessBitterCoder <liangyiyingchen@outlook.com>
2025-08-01 20:43:16 +0800

    递增序列-721

---
+++ CppCode/语法基础课/第三讲-循环语句/6.IncreasingSequence_721/IncreasingSequence.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    while(1)
+    {
+        int x;
+        cin >> x;
+        if(x == 0) break;
+        for(int i = 1; i <= x; i++)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
