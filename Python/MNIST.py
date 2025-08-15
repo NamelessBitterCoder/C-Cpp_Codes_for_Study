9f13193f96edb65affe0d1692c26021961d39451
NamelessBitterCoder <liangyiyingchen@outlook.com>
2025-08-15 23:37:13 +0800

    MNIST.py

---
+++ Python/MNIST.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision import datasets
+import torch.nn.functional as F
+
