48a6252367fe6cf5312ed733cdbe9c4fbb6aef3a
NamelessBitterCoder <liangyiyingchen@outlook.com>
2025-08-20 00:32:25 +0800

    斯皮尔曼相关系数

--- MatlabProject/5_2.SpearmanCorrelation/my_rank_data.m
+++ MatlabProject/5_2.SpearmanCorrelation/my_rank_data.m
@@ -6,4 +6,10 @@ function [RX] = rank_data(X)
 % 举个例子 X = [5 10 9 10 6]'
     [~, index] = sort(X);  % ~表示我们不需要第一个输出值，即排序后的X[5 6 9 10 10]
                            % 这里的 index = [1 5 3 2 4]' 是排序后的X在原向量中的位置 
-    [~, RX] = sort(index);  % 对index进行一次升序，得到的rx就是我们想要额等级 rx = [1 4 3 5 2]'  s
+    [~, RX] = sort(index);  % 对index进行一次升序，得到的rx就是我们想要额等级 rx = [1 4 3 5 2]'
+                            % 等级的定义是：原始向量中每个元素在排序后序列中的位置
+    for i = 1 : size(X, 1)
+        position = (X == X(i));  % 得到X中与X(i)相等的位置，返回一个列向量，向量的值全为1或0
+        RX(position == 1) = sum(RX .* position) / sum(position);  
+    end
+end
